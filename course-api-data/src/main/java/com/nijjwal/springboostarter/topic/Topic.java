7be40dd58762c64e7270f3501d85351c99bee897
Nijjwal Shrestha <nijjwal.shrestha@gmail.com>
2020-02-12 05:44:36 -0700

    Tutorial-28: Added a class that extends CrudRepository

--- course-api-data/src/main/java/com/nijjwal/springboostarter/topic/Topic.java
+++ course-api-data/src/main/java/com/nijjwal/springboostarter/topic/Topic.java
@@ -1,7 +1,12 @@
 package com.nijjwal.springboostarter.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
 
+	@Id
 	private String id;
 	private String name;
 	private String description;
